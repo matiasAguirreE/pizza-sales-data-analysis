da295e3fd9ec892bce46bf9ac514924ef83eb0ad
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-29 00:48:41 -0400

    STYLE: Modified some text to be more understandable

--- sql/queries/02_query.sql
+++ sql/queries/02_query.sql
@@ -1,9 +1,9 @@
 --Question
 --What are the pizzas with the greatest benefits?
-SELECT pizza_id, price * cantidad as BENEFICIOS
+SELECT pizza_id, price * cantidad AS BENEFICIOS
 FROM pizzas, (
     SELECT pizza_id AS nombre, count(pizza_id) AS cantidad
-    FROM order_details Group by pizza_id) as amigo
+    FROM order_details Group by pizza_id) AS subconsulta
 WHERE nombre = pizza_id
 Order by BENEFICIOS desc
 LIMIT 10;
