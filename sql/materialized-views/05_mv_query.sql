43ec7e99a1fcdf011d5965d1977fe0a14419c61f
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-29 01:04:09 -0400

    FEAT: Created materialized view of the query five

---
+++ sql/materialized-views/05_mv_query.sql
@@ -0,0 +1,12 @@
+--Query 5
+CREATE MATERIALIZED VIEW consulta_5 AS
+SELECT mes, SUM(cantidad) AS total_pizzas_vendidas
+FROM (
+    SELECT EXTRACT(MONTH FROM date) AS mes, SUM(quantity) AS cantidad
+    FROM order_details
+    JOIN orders ON order_details.order_id = orders.order_id
+    GROUP BY EXTRACT(MONTH FROM date)
+) AS resumen_meses
+GROUP BY mes
+ORDER BY total_pizzas_vendidas DESC
+LIMIT 1;
