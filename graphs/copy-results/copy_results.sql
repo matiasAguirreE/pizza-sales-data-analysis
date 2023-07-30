26bc56f5f61f96d7ba38daf4a7c23978948bd073
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-29 20:48:36 -0400

    FEAT: Created SQL command to copy the results of all of the six queries

---
+++ graphs/copy-results/copy_results.sql
@@ -0,0 +1,17 @@
+--Query 1
+\copy (SELECT * FROM consulta_1) TO '/home/cc3201/queries-results/01_result.csv' WITH CSV HEADER;
+
+--Query 2
+\copy (SELECT * FROM consulta_2) TO '/home/cc3201/queries-results/02_result.csv' WITH CSV HEADER;
+
+--Query 3
+\copy (SELECT * FROM consulta_3) TO '/home/cc3201/queries-results/03_result.csv' WITH CSV HEADER;
+
+--Query 4
+\copy (SELECT * FROM consulta_4) TO '/home/cc3201/queries-results/04_result.csv' WITH CSV HEADER;
+
+--Query 5
+\copy (SELECT * FROM consulta_5) TO '/home/cc3201/queries-results/05_result.csv' WITH CSV HEADER;
+
+--Query 6
+\copy (SELECT * FROM consulta_6) TO '/home/cc3201/queries-results/06_result.csv' WITH CSV HEADER;
